d552fc218a0c9029898f95609013336567e0ecb0
Saad Ahmed <saadem3000@gmail.com>
2026-01-12 03:28:18 +0000

    update ports

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const path = require('path');
 const fastify = require('fastify')({ logger: true });
 const fastifyStatic = require('@fastify/static');
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 const HOST = process.env.HOST || '0.0.0.0';
 
 // Serve minified JS/CSS from dist/
